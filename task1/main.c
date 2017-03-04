b5bd8bde0cc2341f6b09bd6a5ca9a94c2c42ff6d
ermilova <you@example.com>
2017-03-04 03:02:52 +0300

    hw1

--- task1/main.c
+++ task1/main.c
@@ -1,38 +1,39 @@
 #include<stdio.h>
 #include <malloc.h>
-int main(int argc, char *argv) {
-	int n;
-	int **a;
-	int x1, y1, x2, y2;
 
-	scanf("%d", &n);
-	a = malloc(n * sizeof(int*));
+int main(int argc, char *argv) {
+    int n;
+    int **a;
+    int x1, y1, x2, y2;
+
+    scanf("%d", &n);
+    a = malloc(n * sizeof(int *));
     for (int i = 0; i < n; i++) {
         a[i] = malloc(sizeof(int) * n);
-	}
+    }
 
-	for (int i = 0; i < n; i++) {
+    for (int i = 0; i < n; i++) {
         for (int j = 0; j < n; j++) {
             a[i][j] = (j + 1) * (i + 1);
-		}
-	}
-	
-	scanf("%d", &x1);
-	while(x1 != 0) {
-		scanf("%d %d %d", &y1, &x2, &y2);
-		for (int i = x1 - 1; i < x2; i++) {
+        }
+    }
+
+    scanf("%d", &x1);
+    while (x1 != 0) {
+        scanf("%d %d %d", &y1, &x2, &y2);
+        for (int i = x1 - 1; i < x2; i++) {
             for (int j = y1 - 1; j < y2; j++) {
                 printf("%d ", a[i][j]);
-			}
+            }
             printf("\n");
-        }	
-		scanf("%d", &x1);
-	}
-	
+        }
+        scanf("%d", &x1);
+    }
+
     for (int i = 0; i < n; i++) {
         free(a[i]);
-	}
+    }
 
-	free(a);
-	return 0;
+    free(a);
+    return 0;
 }
